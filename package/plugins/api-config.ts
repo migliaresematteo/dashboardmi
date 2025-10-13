7821584e2fba40254940167727bfb17eb6006870
Matteo Migliarese <96019682+migliaresematteo@users.noreply.github.com>
2025-10-13 08:55:23 +0200

    Update api-config.ts

--- package/plugins/api-config.ts
+++ package/plugins/api-config.ts
@@ -4,7 +4,7 @@ import { setApiConfig } from '~/services/api';
 
 export default defineNuxtPlugin((nuxtApp) => {
   // Configurazione di base delle API
-  const apiBaseUrl = process.env.NUXT_API_BASE_URL || 'https://api.mercatoitinerante.it';
+  const apiBaseUrl = process.env.NUXT_API_BASE_URL || 'https://www.mercatoitinerante.it';
   const apiKey = process.env.NUXT_API_KEY || '';
 
   // Imposta la configurazione delle API
