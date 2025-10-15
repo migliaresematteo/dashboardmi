dbeaefaed486625f39fcc45633ec09df2c6aa051
migliaresematteo <migliaresematteo@gmail.com>
2025-10-15 22:20:51 +0200

    up

--- package/.nuxt/types/nitro-routes.d.ts
+++ package/.nuxt/types/nitro-routes.d.ts
@@ -6,4 +6,9 @@ declare module "nitropack/types" {
     '/api/products': {
       'get': Simplify<Serialize<Awaited<ReturnType<typeof import('../../server/api/products.get').default>>>>
     }
-    '/__nuxt_er
+    '/__nuxt_error': {
+      'default': Simplify<Serialize<Awaited<ReturnType<typeof import('../../node_modules/nuxt/dist/core/runtime/nitro/handlers/renderer').default>>>>
+    }
+  }
+}
+export {}
